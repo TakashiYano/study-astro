411adca8b12c5cae66f2d03bb0221d2d6a356777
yanot <0tp6324w3261z0v@gmail.com>
2023-06-12 16:07:33 +0900

    ✨ Content Collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  output: "server",
+  integrations: [preact()],
+  adapter: netlify(),
 });
